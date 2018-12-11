8847008d1bf7a221b5c01e97506f539ef4f7798c
Shining Sun <shiningsun@google.com>
2018-12-11 19:21:51 +0000

    batch size error

--- official/recommendation/constants.py
+++ official/recommendation/constants.py
@@ -14,8 +14,6 @@
 # ==============================================================================
 """Central location for NCF specific values."""
 
-import sys
-
 import numpy as np
 
 from official.datasets import movielens
@@ -29,7 +27,6 @@ TRAIN_USER_KEY = "train_{}".format(movielens.USER_COLUMN)
 TRAIN_ITEM_KEY = "train_{}".format(movielens.ITEM_COLUMN)
 TRAIN_LABEL_KEY = "train_labels"
 MASK_START_INDEX = "mask_start_index"
-VALID_POINT_MASK = "valid_point_mask"
 EVAL_USER_KEY = "eval_{}".format(movielens.USER_COLUMN)
 EVAL_ITEM_KEY = "eval_{}".format(movielens.ITEM_COLUMN)
 
@@ -37,7 +34,9 @@ USER_MAP = "user_map"
 ITEM_MAP = "item_map"
 
 USER_DTYPE = np.int32
-ITEM_DTYPE = np.int32
+ITEM_DTYPE = np.uint16
+LABEL_DTYPE = np.int8
+DUPE_MASK_DTYPE = np.bool
 
 # In both datasets, each user has at least 20 ratings.
 MIN_NUM_RATINGS = 20
@@ -56,24 +55,11 @@ DUPLICATE_MASK = "duplicate_mask"
 HR_METRIC_NAME = "HR_METRIC"
 NDCG_METRIC_NAME = "NDCG_METRIC"
 
-# Trying to load a cache created in py2 when running in py3 will cause an
-# error due to differences in unicode handling.
-RAW_CACHE_FILE = "raw_data_cache_py{}.pickle".format(sys.version_info[0])
+RAW_CACHE_FILE = "raw_data_cache.pickle"
 CACHE_INVALIDATION_SEC = 3600 * 24
 
 # ==============================================================================
-# == Data Generation ===========================================================
+# == Subprocess Data Generation ================================================
 # ==============================================================================
 CYCLES_TO_BUFFER = 3  # The number of train cycles worth of data to "run ahead"
                       # of the main training loop.
-
-# Number of batches to run per epoch when using synthetic data. At high batch
-# sizes, we run for more batches than with real data, which is good since
-# running more batches reduces noise when measuring the average batches/second.
-SYNTHETIC_BATCHES_PER_EPOCH = 2000
-
-# Only used when StreamingFilesDataset is used.
-NUM_FILE_SHARDS = 16
-TRAIN_FOLDER_TEMPLATE = "training_cycle_{}"
-EVAL_FOLDER = "eval_data"
-SHARD_TEMPLATE = "shard_{}.tfrecords"
--- official/recommendation/data_pipeline.py
+++ official/recommendation/data_pipeline.py
@@ -18,23 +18,17 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import atexit
-import collections
-import functools
+import multiprocessing.dummy
 import os
 import pickle
 import struct
-import sys
-import tempfile
 import threading
 import time
 import timeit
-import traceback
 
 import numpy as np
 from six.moves import queue
 import tensorflow as tf
-from tensorflow.contrib.tpu.python.tpu.datasets import StreamingFilesDataset
 
 from official.datasets import movielens
 from official.recommendation import constants as rconst
@@ -56,183 +50,6 @@ Eval:
 {spacer}Batch size:              {eval_batch_size} {multiplier}
 {spacer}Batch count per epoch:   {eval_batch_ct}"""
 
-
-_TRAIN_FEATURE_MAP = {
-    movielens.USER_COLUMN: tf.FixedLenFeature([], dtype=tf.string),
-    movielens.ITEM_COLUMN: tf.FixedLenFeature([], dtype=tf.string),
-    rconst.MASK_START_INDEX: tf.FixedLenFeature([1], dtype=tf.string),
-    "labels": tf.FixedLenFeature([], dtype=tf.string),
-}
-
-
-_EVAL_FEATURE_MAP = {
-    movielens.USER_COLUMN: tf.FixedLenFeature([], dtype=tf.string),
-    movielens.ITEM_COLUMN: tf.FixedLenFeature([], dtype=tf.string),
-    rconst.DUPLICATE_MASK: tf.FixedLenFeature([], dtype=tf.string)
-}
-
-
-class DatasetManager(object):
-  def __init__(self, is_training, stream_files, batches_per_epoch,
-               shard_root=None):
-    self._is_training = is_training
-    self._stream_files = stream_files
-    self._batches_per_epoch = batches_per_epoch
-    self._epochs_completed = 0
-    self._epochs_requested = 0
-    self._shard_root = shard_root
-
-    self._result_queue = queue.Queue()
-    self._result_reuse = []
-
-  @property
-  def current_data_root(self):
-    subdir = (rconst.TRAIN_FOLDER_TEMPLATE.format(self._epochs_completed)
-              if self._is_training else rconst.EVAL_FOLDER)
-    return os.path.join(self._shard_root, subdir)
-
-  def buffer_reached(self):
-    # Only applicable for training.
-    return (self._epochs_completed - self._epochs_requested >=
-            rconst.CYCLES_TO_BUFFER and self._is_training)
-
-  @staticmethod
-  def _serialize(data):
-    """Convert NumPy arrays into a TFRecords entry."""
-
-    feature_dict = {
-      k: tf.train.Feature(bytes_list=tf.train.BytesList(
-          value=[memoryview(v).tobytes()])) for k, v in data.items()}
-
-    return tf.train.Example(
-        features=tf.train.Features(feature=feature_dict)).SerializeToString()
-
-  def _deserialize(self, serialized_data, batch_size):
-    feature_map = _TRAIN_FEATURE_MAP if self._is_training else _EVAL_FEATURE_MAP
-    features = tf.parse_single_example(serialized_data, feature_map)
-
-    users = tf.reshape(tf.decode_raw(
-        features[movielens.USER_COLUMN], rconst.USER_DTYPE), (batch_size,))
-    items = tf.reshape(tf.decode_raw(
-        features[movielens.ITEM_COLUMN], rconst.ITEM_DTYPE), (batch_size,))
-
-    def decode_binary(data_bytes):
-      # tf.decode_raw does not support bool as a decode type. As a result it is
-      # necessary to decode to int8 (7 of the bits will be ignored) and then
-      # cast to bool.
-      return tf.reshape(tf.cast(tf.decode_raw(data_bytes, tf.int8), tf.bool),
-                        (batch_size,))
-
-    if self._is_training:
-      mask_start_index = tf.decode_raw(
-          features[rconst.MASK_START_INDEX], tf.int32)[0]
-      valid_point_mask = tf.less(tf.range(batch_size), mask_start_index)
-
-      return {
-          movielens.USER_COLUMN: users,
-          movielens.ITEM_COLUMN: items,
-          rconst.VALID_POINT_MASK: valid_point_mask,
-      }, decode_binary(features["labels"])
-
-    return {
-        movielens.USER_COLUMN: users,
-        movielens.ITEM_COLUMN: items,
-        rconst.DUPLICATE_MASK: decode_binary(features[rconst.DUPLICATE_MASK]),
-    }
-
-  def put(self, index, data):
-    # type: (int, dict) -> None
-    if self._stream_files:
-      example_bytes = self._serialize(data)
-      shard_name = rconst.SHARD_TEMPLATE.format(index % rconst.NUM_FILE_SHARDS)
-      fpath = os.path.join(self.current_data_root, shard_name)
-      with tf.python_io.TFRecordWriter(fpath) as writer:
-        writer.write(example_bytes)
-    else:
-      if self._is_training:
-        mask_start_index = data.pop(rconst.MASK_START_INDEX)
-        batch_size = data[movielens.ITEM_COLUMN].shape[0]
-        data[rconst.VALID_POINT_MASK] = np.less(np.arange(batch_size),
-                                                mask_start_index)
-        self._result_queue.put((data, data.pop("labels")))
-      else:
-        self._result_reuse.append(data)
-
-  def start_construction(self):
-    if self._stream_files:
-      tf.gfile.MakeDirs(self.current_data_root)
-
-  def end_construction(self):
-    if self._stream_files:
-      self._result_queue.put(self.current_data_root)
-    elif not self._is_training:
-      self._result_queue.put(True)  # data is ready.
-
-    self._epochs_completed += 1
-
-  def data_generator(self):
-    assert not self._stream_files
-
-    if self._is_training:
-      for _ in range(self._batches_per_epoch):
-        yield self._result_queue.get(timeout=300)
-
-    else:
-      # Evaluation waits for all data to be ready.
-      self._result_queue.put(self._result_queue.get(timeout=300))
-      assert len(self._result_reuse) == self._batches_per_epoch
-      for i in self._result_reuse:
-        yield i
-
-  def get_dataset(self, batch_size):
-    self._epochs_requested += 1
-    if self._stream_files:
-      epoch_data_dir = self._result_queue.get(timeout=300)
-      if not self._is_training:
-        self._result_queue.put(epoch_data_dir)  # Eval data is reused.
-
-      file_pattern = os.path.join(
-          epoch_data_dir, rconst.SHARD_TEMPLATE.format("*"))
-      dataset = StreamingFilesDataset(
-          files=file_pattern, worker_job="worker",
-          num_parallel_reads=rconst.NUM_FILE_SHARDS)
-      return dataset.map(functools.partial(self._deserialize, batch_size=batch_size), num_parallel_calls=16)
-
-    types = {movielens.USER_COLUMN: rconst.USER_DTYPE,
-             movielens.ITEM_COLUMN: rconst.ITEM_DTYPE}
-    shapes = {movielens.USER_COLUMN: tf.TensorShape([batch_size]),
-              movielens.ITEM_COLUMN: tf.TensorShape([batch_size])}
-
-    if self._is_training:
-      types[rconst.VALID_POINT_MASK] = np.bool
-      shapes[rconst.VALID_POINT_MASK] = tf.TensorShape([batch_size])
-
-      types = (types, rconst.LABEL_DTYPE)
-      shapes = (shapes, tf.TensorShape([batch_size]))
-
-    else:
-      types[rconst.DUPLICATE_MASK] = rconst.DUPE_MASK_DTYPE
-      shapes[rconst.DUPLICATE_MASK] = tf.TensorShape([batch_size])
-
-    return tf.data.Dataset.from_generator(
-        generator=self.data_generator, output_types=types, output_shapes=shapes)
-
-  def make_input_fn(self, batch_size):
-    def input_fn(params):
-      param_batch_size = (params["batch_size"] if self._is_training else
-                          params["eval_batch_size"])
-      if batch_size != param_batch_size:
-        raise ValueError("producer batch size ({}) differs from params batch "
-                         "size ({})".format(batch_size, param_batch_size))
-
-      dataset = self.get_dataset(batch_size=batch_size)
-      dataset = dataset.prefetch(16)
-
-      return dataset
-
-    return input_fn
-
-
 class BaseDataConstructor(threading.Thread):
   def __init__(self,
                maximum_number_epochs,   # type: int
@@ -247,33 +64,28 @@ class BaseDataConstructor(threading.Thread):
                eval_pos_items,          # type: np.ndarray
                eval_batch_size,         # type: int
                batches_per_eval_step,   # type: int
-               stream_files             # type: bool
               ):
     # General constants
     self._maximum_number_epochs = maximum_number_epochs
     self._num_users = num_users
     self._num_items = num_items
-    self._train_pos_users = train_pos_users
-    self._train_pos_items = train_pos_items
-    self.train_batch_size = train_batch_size
-    self._num_train_negatives = num_train_negatives
-    self._batches_per_train_step = batches_per_train_step
-    self._eval_pos_users = eval_pos_users
-    self._eval_pos_items = eval_pos_items
-    self.eval_batch_size = eval_batch_size
 
     # Training
-    if self._train_pos_users.shape != self._train_pos_items.shape:
-      raise ValueError("User training positives ({}) is different from item "
-                       "training positives ({})".format(
-          self._train_pos_users.shape, self._train_pos_items.shape))
-
+    self._train_pos_users = train_pos_users
+    self._train_pos_items = train_pos_items
+    assert self._train_pos_users.shape == self._train_pos_items.shape
     self._train_pos_count = self._train_pos_users.shape[0]
+    self.train_batch_size = train_batch_size
+    self._num_train_negatives = num_train_negatives
     self._elements_in_epoch = (1 + num_train_negatives) * self._train_pos_count
+    self._batches_per_train_step = batches_per_train_step
     self.train_batches_per_epoch = self._count_batches(
         self._elements_in_epoch, train_batch_size, batches_per_train_step)
 
     # Evaluation
+    self._eval_pos_users = eval_pos_users
+    self._eval_pos_items = eval_pos_items
+    self.eval_batch_size = eval_batch_size
     if eval_batch_size % (1 + rconst.NUM_EVAL_NEGATIVES):
       raise ValueError("Eval batch size {} is not divisible by {}".format(
           eval_batch_size, 1 + rconst.NUM_EVAL_NEGATIVES))
@@ -288,23 +100,28 @@ class BaseDataConstructor(threading.Thread):
     self._shuffle_producer = stat_utils.AsyncPermuter(
         self._elements_in_epoch, num_workers=3,
         num_to_produce=maximum_number_epochs)
-
-    if stream_files:
-      self._shard_root = tempfile.mkdtemp(prefix="ncf_")
-      atexit.register(tf.gfile.DeleteRecursively, dirname=self._shard_root)
-    else:
-      self._shard_root = None
-
-    self._train_dataset = DatasetManager(
-        True, stream_files, self.train_batches_per_epoch, self._shard_root)
-    self._eval_dataset = DatasetManager(
-        False, stream_files, self.eval_batches_per_epoch, self._shard_root)
+    self._training_queue = queue.Queue()
+    self._eval_results = None
+    self._eval_batches = None
 
     # Threading details
     self._current_epoch_order_lock = threading.Lock()
     super(BaseDataConstructor, self).__init__()
     self.daemon = True
     self._stop_loop = False
+
+    # Generator annotations
+    self.train_gen_types = (rconst.USER_DTYPE, rconst.ITEM_DTYPE,
+                            rconst.LABEL_DTYPE, np.int32)
+    self.train_gen_shapes = (tf.TensorShape([self.train_batch_size]),
+                             tf.TensorShape([self.train_batch_size]),
+                             tf.TensorShape([self.train_batch_size]),
+                             tf.TensorShape([]))
+
+    self.eval_gen_types = (rconst.USER_DTYPE, rconst.ITEM_DTYPE,
+                           rconst.DUPE_MASK_DTYPE)
+    self.eval_gen_shapes = tuple([tf.TensorShape([self.eval_batch_size])
+                                  for _ in range(3)])
 
   def __repr__(self):
     summary = SUMMARY_TEMPLATE.format(
@@ -318,8 +135,7 @@ class BaseDataConstructor(threading.Thread):
         self._batches_per_train_step > 1 else "")
     return super(BaseDataConstructor, self).__repr__() + "\n" + summary
 
-  @staticmethod
-  def _count_batches(example_count, batch_size, batches_per_step):
+  def _count_batches(self, example_count, batch_size, batches_per_step):
     x = (example_count + batch_size - 1) // batch_size
     return (x + batches_per_step - 1) // batches_per_step * batches_per_step
 
@@ -346,7 +162,7 @@ class BaseDataConstructor(threading.Thread):
   def lookup_negative_items(self, **kwargs):
     raise NotImplementedError
 
-  def _run(self):
+  def run(self):
     self._shuffle_producer.start()
     self.construct_lookup_variables()
     self._construct_training_epoch()
@@ -354,17 +170,7 @@ class BaseDataConstructor(threading.Thread):
     for _ in range(self._maximum_number_epochs - 1):
       self._construct_training_epoch()
 
-  def run(self):
-    try:
-      self._run()
-    except Exception:
-      # The Thread base class swallows stack traces, so unfortunately it is
-      # necessary to catch and re-raise to get debug output
-      print(traceback.format_exc(), file=sys.stderr)
-      sys.stderr.flush()
-      raise
-
-  def _get_training_batch(self, i):
+  def _get_training_batch(self, _):
     batch_indices = self._get_order_chunk()
 
     batch_ind_mod = np.mod(batch_indices, self._train_pos_count)
@@ -393,16 +199,12 @@ class BaseDataConstructor(threading.Thread):
       items = np.concatenate([items, item_pad])
       labels = np.concatenate([labels, label_pad])
 
-    self._train_dataset.put(i, {
-      movielens.USER_COLUMN: users,
-      movielens.ITEM_COLUMN: items,
-      rconst.MASK_START_INDEX: np.array(batch_indices.shape[0], dtype=np.int32),
-      "labels": labels,
-    })
+    self._training_queue.put((users, items, labels, batch_indices.shape[0]))
 
   def _wait_to_construct_train_epoch(self):
+    threshold = rconst.CYCLES_TO_BUFFER * self.train_batches_per_epoch
     count = 0
-    while self._train_dataset.buffer_reached() and not self._stop_loop:
+    while self._training_queue.qsize() >= threshold and not self._stop_loop:
       time.sleep(0.01)
       count += 1
       if count >= 100 and np.log10(count) == np.round(np.log10(count)):
@@ -411,18 +213,15 @@ class BaseDataConstructor(threading.Thread):
 
   def _construct_training_epoch(self):
     self._wait_to_construct_train_epoch()
-    start_time = timeit.default_timer()
     if self._stop_loop:
       return
 
-    self._train_dataset.start_construction()
-    map_args = list(range(self.train_batches_per_epoch))
+    start_time = timeit.default_timer()
+    map_args = [i for i in range(self.train_batches_per_epoch)]
     assert not self._current_epoch_order.shape[0]
     self._current_epoch_order = self._shuffle_producer.get()
-
     with popen_helper.get_threadpool(6) as pool:
       pool.map(self._get_training_batch, map_args)
-    self._train_dataset.end_construction()
 
     tf.logging.info("Epoch construction complete. Time: {:.1f} seconds".format(
       timeit.default_timer() - start_time))
@@ -463,31 +262,32 @@ class BaseDataConstructor(threading.Thread):
 
     assert users.shape == items.shape == duplicate_mask.shape
 
-    self._eval_dataset.put(i, {
-        movielens.USER_COLUMN: users.flatten(),
-        movielens.ITEM_COLUMN: items.flatten(),
-        rconst.DUPLICATE_MASK: duplicate_mask.flatten(),
-    })
+    return users.flatten(), items.flatten(), duplicate_mask.flatten()
 
   def _construct_eval_epoch(self):
     if self._stop_loop:
       return
 
     start_time = timeit.default_timer()
-
-    self._eval_dataset.start_construction()
     map_args = [i for i in range(self.eval_batches_per_epoch)]
     with popen_helper.get_threadpool(6) as pool:
-      pool.map(self._get_eval_batch, map_args)
-    self._eval_dataset.end_construction()
+      eval_results = pool.map(self._get_eval_batch, map_args)
+
+    self._eval_results = eval_results
 
     tf.logging.info("Eval construction complete. Time: {:.1f} seconds".format(
         timeit.default_timer() - start_time))
 
-  def make_input_fn(self, is_training):
-    return (
-      self._train_dataset.make_input_fn(self.train_batch_size) if is_training
-      else self._eval_dataset.make_input_fn(self.eval_batch_size))
+  def training_generator(self):
+    for _ in range(self.train_batches_per_epoch):
+      yield self._training_queue.get(timeout=300)
+
+  def eval_generator(self):
+    while self._eval_results is None:
+      time.sleep(0.01)
+
+    for i in self._eval_results:
+      yield i
 
 
 class DummyConstructor(threading.Thread):
@@ -496,45 +296,6 @@ class DummyConstructor(threading.Thread):
 
   def stop_loop(self):
     pass
-
-  def make_input_fn(self, is_training):
-    """Construct training input_fn that uses synthetic data."""
-
-    def input_fn(params):
-      """Generated input_fn for the given epoch."""
-      batch_size = (params["batch_size"] if is_training else
-                    params["eval_batch_size"] or params["batch_size"])
-      num_users = params["num_users"]
-      num_items = params["num_items"]
-
-      users = tf.random_uniform([batch_size], dtype=tf.int32, minval=0,
-                                maxval=num_users)
-      items = tf.random_uniform([batch_size], dtype=tf.int32, minval=0,
-                                maxval=num_items)
-
-      if is_training:
-        labels = tf.random_uniform([batch_size], dtype=tf.int32, minval=0,
-                                   maxval=2)
-        data = {
-                 movielens.USER_COLUMN: users,
-                 movielens.ITEM_COLUMN: items,
-                 rconst.MASK_START_INDEX: tf.convert_to_tensor(batch_size),
-               }, labels
-      else:
-        dupe_mask = tf.cast(tf.random_uniform([batch_size], dtype=tf.int32,
-                                              minval=0, maxval=2), tf.bool)
-        data = {
-          movielens.USER_COLUMN: users,
-          movielens.ITEM_COLUMN: items,
-          rconst.DUPLICATE_MASK: dupe_mask,
-        }
-
-      dataset = tf.data.Dataset.from_tensors(data).repeat(
-          rconst.SYNTHETIC_BATCHES_PER_EPOCH)
-      dataset = dataset.prefetch(32)
-      return dataset
-
-    return input_fn, rconst.SYNTHETIC_BATCHES_PER_EPOCH
 
 
 class MaterializedDataConstructor(BaseDataConstructor):
